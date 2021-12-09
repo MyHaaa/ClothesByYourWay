37bcefb60bfa553d7405bedb5b6ac6e9c6721d58
Huynh Thi Kim Tien <18dh110190@st.huflit.edu.vn>
2021-12-09 09:15:06 +0700

    Customer update

--- ClothesBYW/Controllers/ShoppingCartController.cs
+++ ClothesBYW/Controllers/ShoppingCartController.cs
@@ -80,5 +80,90 @@ namespace ClothesBYW.Controllers
             cart.ClearCart();
             return RedirectToAction("Showcart", "ShoppingCart");
         }
+        public string GuidId()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[10];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            return finalString;
+        }
+        public ActionResult CheckOut(FormCollection form)
+        {
+            try
+            {
+                Models.GioHang cart = Session["Models.GioHang"] as Models.GioHang;
+                // Khach hang moi
+                /* IDEA
+                 * Tạo 1 hàm, trong đó đầu vào là 1 chuỗi cũ (gọi là chuỗi x)
+                 * (VD: KH0001 -> Lấy từ db.TAIKHOAN.FindLast)
+                 * x.Subtring (0,2) -> chuỗi tĩnh
+                 * convert.ToInt(x.substring(2,last) -> số i cũ
+                 * Tăng i++
+                 * Convert i mới sang chuỗi 4 ký tự
+                 * kết quả= chuỗi tỉnh + chuỗi i
+                 */
+                Customer kh = new Customer();
+                kh.CustomerCatergoryID = this.GuidId();//
+                kh.DateOfBirth = DateTime.Now;
+
+                kh.Name = form["TenKhachHang"];
+                kh.Phone = form["DienThoaiKhach"];
+                kh.Email = form["Email"];
+                Order order = new Order();
+                order.CustomerID = kh.CustomerID;
+                db.Customers.Add(kh);
+
+                order.OrderID = this.GuidId();//
+                order.AddressShip = form["DiaChiKhachHang"];
+                order.DeliveryDatetime = DateTime.Now;
+                db.Orders.Add(order);
+
+                foreach (var item in cart.Items)
+                {
+
+                    OrderDetail order_detail = new OrderDetail();
+                    //order_detail.ID = i.ToString();
+                    order_detail.OrderID = order.OrderID;
+                    order_detail.ProductID = item.sp.ProductID;
+                    order_detail.OrderDetailID = this.GuidId();//
+                    order_detail.Product.Prices = (float)item.sp.Prices.First().RetailPrice;
+                    order_detail.QuantitySold = item.SoLuong;
+                    db.OrderDetails.Add(order_detail);
+                    foreach (var p in db.Products.Where(s => s.ProductID == order_detail.ProductID))
+                    {
+                        var update_quan_pro = p.QuantityInStock - item.SoLuong;
+                        p.QuantityInStock = update_quan_pro;
+
+                    }
+
+                }
+
+                db.SaveChanges();
+                cart.ClearCart();
+
+                return RedirectToAction("Check_out_success", "ShoppingCart");
+            }
+            catch (Exception e)
+            {
+                return Content("Error checkout. Please check information customer...");
+            }
+
+
+        }
+        public ActionResult FormTTKH()
+        {
+            return View();
+        }
+        public ActionResult Check_out_success()
+        {
+            return View();
+        }
     }
 }
