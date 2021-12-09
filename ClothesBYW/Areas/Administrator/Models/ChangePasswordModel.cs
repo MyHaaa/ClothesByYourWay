5d57b57bfc610e4cc6dff4157f1c3aa5cf5c9a2d
MyHa <74403666+MyHaaa@users.noreply.github.com>
2021-12-10 05:34:31 +0700

    Update Image & Change Pass for Current Employee Account

---
+++ ClothesBYW/Areas/Administrator/Models/ChangePasswordModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace ClothesBYW.Areas.Administrator.Models
+{
+    public class ChangePasswordModel
+    {
+        [Key]
+        [Display(Name = "Mật khẩu cũ")]
+        [Required(ErrorMessage = "Bạn chưa nhập mật khẩu cũ!")]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+
+        [Display(Name = "Mật khẩu mới")]
+        [Required(ErrorMessage = "Bạn chưa nhập mật khẩu mới!")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Display(Name = "Nhập lại mật khẩu mới")]
+        [Required(ErrorMessage = "Bạn chưa nhập lại mật khẩu mới!")]
+        [Compare(otherProperty: "NewPassword", ErrorMessage = "Mật khẩu nhập lại không trùng khớp!")]
+        [DataType(DataType.Password)]
+        public string ConfirmNewPassword { get; set; }
+
+    }
+}
+}
